cee26e5ef59f26f310c8ce45d05498faff187928
Vishal <vivanpanchal9540@gmail.com>
2024-12-02 17:30:44 +0530

    write routes and controllers

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import connectDB from "./db/db.js";
 import { app } from "./app.js";
 
 
-
 dotenv.config({ path: "./.env" });
-
 
 connectDB().then(() => {
 	app.listen(process.env.PORT || 3000, () => {
 		console.log(`server is running at port : ${process.env.PORT}`)
 	});
-	app.get('/', (req, res) => {
-		res.send('Hello, World!');
-	});
-
 }).catch((err) => {
 	console.log(`error : ${err}`)
 })
